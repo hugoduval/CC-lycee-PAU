e02b4aaf5f55ae5dd02a88a3ff8214841084bdbe
hugoduval <72018966+hugoduval@users.noreply.github.com>
2022-11-30 19:48:24 +0000

    ex04

--- exos/ex04/ex04.py
+++ exos/ex04/ex04.py
@@ -1,1 +1,7 @@
+cadeaux = ["bleu", "rouge", "vert", "jaune", "jaune", "rouge", "rouge", "rouge", "bleu", "bleu", "rouge", "bleu", "rouge", "vert", "bleu","bleu", "vert", "vert", "jaune", "rouge", "jaune", "rouge", "jaune", "rouge", "rouge", "rouge", "vert", "vert", "vert"]
 
+nb = 0
+
+# Compter le nombre de fois que "rouge" apparaît dans la liste avec nb
+
+print(f"nombre de cadeaux à refaire = {nb}")
