73ccf8cf27a555fe039db72ba6f8edb5ce740a46
hugoduval <72018966+hugoduval@users.noreply.github.com>
2022-11-30 20:32:14 +0100

    Update ex04.py

--- exos/ex04/ex04.py
+++ exos/ex04/ex04.py
@@ -1,7 +1,1 @@
-cadeaux = ["bleu", "rouge", "vert", "jaune", "jaune", "rouge", "rouge", "rouge", "bleu", "bleu", "rouge", "bleu", "rouge", "vert", "bleu","bleu", "vert", "vert", "jaune", "rouge", "jaune", "rouge", "jaune", "rouge", "rouge", "rouge", "vert", "vert", "vert"]
-bleu = []
-rouge = []
-jaune = []
-vert = []
 
-#triez les cadeaux en fonction de leur couleur
