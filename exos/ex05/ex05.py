cef978d5e2025b700308882a517d71bb3fb62afc
hugoduval <72018966+hugoduval@users.noreply.github.com>
2022-11-30 20:32:35 +0100

    Update ex05.py

---
+++ exos/ex05/ex05.py
@@ -0,0 +1,7 @@
+cadeaux = ["bleu", "rouge", "vert", "jaune", "jaune", "rouge", "rouge", "rouge", "bleu", "bleu", "rouge", "bleu", "rouge", "vert", "bleu","bleu", "vert", "vert", "jaune", "rouge", "jaune", "rouge", "jaune", "rouge", "rouge", "rouge", "vert", "vert", "vert"]
+bleu = []
+rouge = []
+jaune = []
+vert = []
+
+#triez les cadeaux en fonction de leur couleur
